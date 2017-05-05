eb7bdeb837347592093c206498926d45e826e8ed
Guillaume EHRET <guillaume@dev-mind.fr>
2017-05-05 20:10:49 +0200

    Adds more sharing on social network

--- src/js/blog.js
+++ src/js/blog.js
@@ -111,7 +111,16 @@ window.blog = (function() {
 
   function sendMessage(target, page, title){
     if('twitter' === target){
-      document.location.href=`https://twitter.com/intent/tweet?original_referer=${encodeURI(page)}&text=${encodeURI(title) + '@DevMindFr'}&tw_p=tweetbutton&url=${encodeURI(page)}`;
+      document.location.href=`https://twitter.com/intent/tweet?original_referer=${encodeURI(page)}&text=${encodeURI(title) + ' @DevMindFr'}&tw_p=tweetbutton&url=${encodeURI(page)}`;
+    }
+    else if('google+' === target){
+      document.location.href=`https://plus.google.com/share?url=${encodeURI(page)}&text=${encodeURI(title)}`;
+    }
+    else if('linkedin' === target){
+      document.location.href=`www.linkedin.com/shareArticle?mini=true&url=${encodeURI(page)}&text=${encodeURI(title)}`;
+    }
+    else if('facebook' === target){
+      document.location.href=`https://www.facebook.com/sharer/sharer.php?u=${encodeURI(page)}&description=${encodeURI(title)}`;
     }
   }
 
