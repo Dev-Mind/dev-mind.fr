573210d45b2b8969b081aa3802e67be838ca980c
Guillaume EHRET <guillaume@dev-mind.fr>
2017-05-12 10:15:28 +0200

    Fix bug with blog directory

--- gulp-extensions/transformers/asciidoctor-read.js
+++ gulp-extensions/transformers/asciidoctor-read.js
@@ -32,6 +32,7 @@ module.exports = function ({ includes } = {}) {
       contents: () => file.contents,
       'github-edit-url': () => file.git.githubEditUrl,
       filename: file.path.substring(file.path.lastIndexOf("/") + 1, file.path.lastIndexOf(".")),
+      dir: file.path.substring(file.path.lastIndexOf("blog/") + 5, file.path.lastIndexOf("/")),
       category: file.attributes.category,
       teaser: file.attributes.teaser,
       imgteaser: file.attributes.imgteaser
