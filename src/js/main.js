70bf30bd7982644d176407226b520d9fbfdd2d23
Guillaume EHRET <guillaume@dev-mind.fr>
2018-01-08 13:44:02 +0100

    Updates service worker configuration

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,7 @@ window.app = (function() {
   'use strict';
 
   function initSw(){
-    //console.log('init service worker with path ' + path);
-    // Check to make sure service workers are supported in the current browser,
-    // and that the current page is accessed from a secure origin. Using a
-    // service worker from an insecure origin will trigger JS console errors. See
-    // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
+
     var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
       // [::1] is the IPv6 localhost address.
       window.location.hostname === '[::1]' ||
@@ -18,15 +14,27 @@ window.app = (function() {
     );
 
     if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || isLocalhost)) {
+      navigator.serviceWorker
+        .register(`sw.js`)
+        .then(function (registration) {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope)
+          registration.onupdatefound = function () {
+            var installingWorker = registration.installing;
+            installingWorker.onstatechange = function () {
+              switch (installingWorker.state) {
+                case 'installed':
+                  if (navigator.serviceWorker.controller) {
+                    console.log('new update available');
+                    location.reload(true);
+                  }
+                  break;
 
-      if(BroadcastChannel){
-        const precacheUpdates = new BroadcastChannel('precache-updates');
-        precacheUpdates.addEventListener('message', (event) => {
-          console.debug(`${event.data} was updated. The new value will be used the next time a request is made.`);
-        });
-      }
-
-      navigator.serviceWorker.register(`sw.js`).catch(function(e) {
+                default:
+              }
+            }
+          }
+        })
+        .catch(function(e) {
           console.error('Error during service worker registration:', e);
       });
     }
--- src/sw.js
+++ src/sw.js
@@ -10,6 +10,7 @@ workboxSW.router.registerRoute('https://cdn.polyfill.io/v2/polyfill.min.js',
     workboxSW.strategies.cacheFirst({
       cacheName: 'googleapis',
       cacheExpiration: {
+        networkTimeoutSeconds: 3,
         maxEntries: 20
       },
       cacheableResponse: {statuses: [0, 200]}
@@ -20,6 +21,7 @@ workboxSW.router.registerRoute('https://fonts.googleapis.com/(.*)',
   workboxSW.strategies.cacheFirst({
     cacheName: 'googleapis',
     cacheExpiration: {
+      networkTimeoutSeconds: 3,
       maxEntries: 20
     },
     cacheableResponse: {statuses: [0, 200]}
@@ -27,9 +29,10 @@ workboxSW.router.registerRoute('https://fonts.googleapis.com/(.*)',
 );
 // We want no more than 100 images in the cache. We check using a cache first strategy
 workboxSW.router.registerRoute(/\.(?:png|gif|jpg)$/,
-  workboxSW.strategies.cacheFirst({
+  workboxSW.strategies.networkFirst({
     cacheName: 'images-cache',
     cacheExpiration: {
+      networkTimeoutSeconds: 3,
       maxEntries: 100
     }
   })
@@ -37,10 +40,11 @@ workboxSW.router.registerRoute(/\.(?:png|gif|jpg)$/,
 workboxSW.router.registerRoute('/(.*)',
   workboxSW.strategies.networkFirst({
     cacheName: 'general-cache',
-    'cacheExpiration': {
+    cacheExpiration: {
       networkTimeoutSeconds: 3,
       maxAgeSeconds: 7200
     },
+    cacheableResponse: {statuses: [0, 200]},
     broadcastCacheUpdate: {
       channelName: 'precache-updates'
     }
