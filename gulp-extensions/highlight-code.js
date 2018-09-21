598d1a78797a5c7d2e724934611a464f98509dc9
Guillaume EHRET Dev-Mind <guillaume@dev-mind.fr>
2018-09-21 15:30:47 +0200

    Use prism.js in place of highlight.js. Use gulp 4 watcher

--- gulp-extensions/highlight-code.js
+++ gulp-extensions/highlight-code.js
@@ -1,19 +1,20 @@
 'use strict';
 
 const map = require('map-stream');
-const Highlights = require('highlights');
-const highlighter = new Highlights();
+const Prism = require('node-prismjs');
 const cheerio = require('cheerio');
 
 module.exports = function ({ selector }) {
 
   return map((file, next) => {
     const $ = cheerio.load(file.contents.toString(), { decodeEntities: false });
+
     $(selector).each((index, code) => {
       const elem = $(code);
+      const language = elem.prop('data-lang');
       const fileContents = elem.html();
-      const highlightedContents = highlighter.highlightSync({ fileContents, scopeName: 'source.js' });
-      elem.parent().replaceWith(highlightedContents);
+      const highlightedContents = Prism.highlight(fileContents,  Prism.languages[language] || Prism.languages.autoit);
+      elem.parent().replaceWith( `<pre class="language-${language}">${highlightedContents}</pre>`);
       elem.addClass('highlights');
     });
 
