7af8e51f8c6d88116979cdc54f8c2bacd2ff170a
Guillaume EHRET Dev-Mind <guillaume@dev-mind.fr>
2018-09-25 16:25:19 +0200

    Updates gradle and springs files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const http = require('http');
+const http2 = require('http2');
+const fs = require('vinyl-fs');
 const compression = require('compression');
 const helmet = require('helmet');
 const cachePolicy = require('./app.webcache');
@@ -9,8 +10,8 @@ const session = require('express-session');
 const parseJsonEnv = (value) => {
   const fields = value.replace("{", "").replace("}", "").replace("[", "").replace("]", "").trim().split(",")
   const users = [];
-  for(let i=0 ; i< fields.length ; i = i+2){
-    users.push({ username: fields[0].split(":")[1].trim(), password: fields[1].split(":")[1].trim()});
+  for (let i = 0; i < fields.length; i = i + 2) {
+    users.push({username: fields[0].split(":")[1].trim(), password: fields[1].split(":")[1].trim()});
   }
   return users
 };
@@ -20,34 +21,41 @@ const DEVMIND = {
   port: process.env.PORT || 8081,
   secret: process.env.DEVMIND_SESSION_SECRET || 'SMHQs7cLAC3x',
   securedUrls: ['/training/'],
-  users : process.env.DEVMIND_USERS ? parseJsonEnv(process.env.DEVMIND_USERS) : [{username: 'guillaume', password: '5f4dcc3b5aa765d61d8327deb882cf99'}]
+  users: process.env.DEVMIND_USERS ? parseJsonEnv(process.env.DEVMIND_USERS) : [{
+    username: 'guillaume',
+    password: '5f4dcc3b5aa765d61d8327deb882cf99'
+  }]
 };
 
 const app = express()
   .use(session(security.sessionAttributes(DEVMIND.secret)))
   .use(compression())
-  .use(express.urlencoded({ extended: false }))
+  .use(express.urlencoded({extended: false}))
   .use(helmet())
   //.use(helmet.contentSecurityPolicy(security.securityPolicy()))
   .use(security.checkAuth(DEVMIND.securedUrls))
-  .use(express.static(DEVMIND.static, { setHeaders: cachePolicy.setCustomCacheControl }))
+  .use(express.static(DEVMIND.static, {setHeaders: cachePolicy.setCustomCacheControl}))
   .get('/logout', security.logoutHandler())
   .post('/login', security.loginHandler(DEVMIND.users))
   .all('*', security.notFoundHandler());
 
 app.set('port', DEVMIND.port);
 
-http.Server(app)
-    .listen(DEVMIND.port)
-    .on('error', onError)
-    .on('listening', () => {
-      console.debug('Listening on ' + DEVMIND.port);
-      console.debug('Config ', DEVMIND);
-    });
-
+const HTTPS_CONFIG = {
+  key: fs.readFileSync('localhost-privkey.pem'),
+  cert: fs.readFileSync('localhost-cert.pem')
+};
+http2.createSecureServer(HTTPS_CONFIG)
+     .listen(DEVMIND.port)
+     .on('error', onError)
+     .on('listening', () => {
+       console.debug(`Listening on ${DEVMIND.port}`);
+       console.debug(`Environnement ${process.env.NODE_ENV}`);
+       console.debug(`Users ${DEVMIND.users.map(u => u.username)}`);
+     });
 
 function onError(error) {
-  console.error("Unxpected error " + error.syscall)
+  console.error(error);
   if (error.syscall !== 'listen') {
     throw error;
   }
--- src/js/main.js
+++ src/js/main.js
@@ -42,22 +42,13 @@ window.app = (function() {
 
   function changeMenu(section){
     if(!section){
-      if(window.location.href.indexOf('blog') > 0){
-        section = '#blog';
-      }
-      else if(window.location.href.indexOf('experience') > 0){
-        section = '#experience';
-      }
-      else if(window.location.href.indexOf('formation') > 0){
-        section = '#formation';
-      }
-      else{
-        section = '#home';
-      }
+      const paths = ['blog', 'experience', 'formation', 'training'].filter(path => window.location.href.indexOf(path) > 0);
+      section = paths.length > 0 ? '#' + paths[0] : '#home';
     }
     if(section === '#casino' || section === '#iorga' || section === '#boiron' || section === '#solutec' || section ==='#conferences'){
       section = '#experience';
     }
+    console.log(section)
     if(lastActiveSection && document.getElementById(lastActiveSection)){
       document.getElementById(lastActiveSection).classList.remove("is-active");
     }
