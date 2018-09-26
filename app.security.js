e2221050f1943abf7222ff8391d2febbe9a99569
Guillaume EHRET Dev-Mind <guillaume@dev-mind.fr>
2018-09-26 11:49:54 +0200

    Redirection on domain without www don't work. So this feature is disabled

--- app.security.js
+++ app.security.js
@@ -57,10 +57,11 @@ exports.rewrite = () => {
         console.log('User is not in HTTP, he is redirected');
         res.redirect('https://dev-mind.fr' + req.url);
       }
-      else if((isHtmlPage || req.url === '/')  && hostWwwInHeader){
-        console.log('User is not on www, he is redirected');
-        res.redirect('https://dev-mind.fr/index.html');
-      }
+      // Redirection on domain without www don't work. So this feature is disabled
+      // else if((isHtmlPage || req.url === '/')  && hostWwwInHeader){
+      //   console.log('User is not on www, he is redirected');
+      //   res.redirect('https://dev-mind.fr/index.html');
+      // }
       else{
         next();
       }
