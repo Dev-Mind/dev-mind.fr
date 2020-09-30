324236d4bcae3de60135d91869605bc738a7d4c9
Guillaume EHRET <guillaume@dev-mind.fr>
2020-09-30 09:50:49 +0200

    Remove facebook link from my website

--- src/main/client/js/bloglist.js
+++ src/main/client/js/bloglist.js
@@ -50,14 +50,8 @@ window.blog = (function () {
     if ('twitter' === target) {
       document.location.href = `https://twitter.com/intent/tweet?original_referer=${encodeURI(page)}&text=${encodeURI(title)}&tw_p=tweetbutton&url=${encodeURI(page)}`;
     }
-    else if ('google+' === target) {
-      document.location.href = `https://plus.google.com/share?url=${encodeURI(page)}&text=${encodeURI(title)}`;
-    }
     else if ('linkedin' === target) {
       document.location.href = `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURI(page)}&text=${encodeURI(title)}`;
-    }
-    else if ('facebook' === target) {
-      document.location.href = `https://www.facebook.com/sharer/sharer.php?u=${encodeURI(page)}&description=${encodeURI(title)}`;
     }
   }
 
